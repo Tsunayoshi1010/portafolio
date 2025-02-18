1aca7eb68bf9d7f0a7d41314101b1e25efbcb986
hover10tsuna@outlook.com <hover10tsuna@outlook.com>
2025-02-18 08:56:12 -0500

    actualizacion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutMeComponent } from './home/about-me/about-me.component';
+import { ContactComponent } from './home/contact/contact.component';
+import { ProjectsComponent } from './home/projects/projects.component';
+import { SkillsComponent } from './home/skills/skills.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent, // RUTA PRINCIPAL
+    children: [
+      { path: 'about-me', component: AboutMeComponent },
+      { path: 'contact', component: ContactComponent },
+      { path: 'projects', component: ProjectsComponent },
+      { path: 'skills', component: SkillsComponent },
+      { path: '', redirectTo: 'about-me', pathMatch: 'full' }, // Si la URL está vacía, redirigir a "home"
+      { path: '**', component: NotFoundComponent } // Ruta por defecto dentro de "home"
+    ]
+  },
+  { path: '', redirectTo: 'home', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule, provideClientHydration, withEventReplay } from '@angular
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutMeComponent } from './home/about-me/about-me.component';
+import { ProjectsComponent } from './home/projects/projects.component';
+import { SkillsComponent } from './home/skills/skills.component';
+import { ContactComponent } from './home/contact/contact.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutMeComponent,
+    ProjectsComponent,
+    SkillsComponent,
+    ContactComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
